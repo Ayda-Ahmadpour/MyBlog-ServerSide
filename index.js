6919bd312242f56ddab4c3e05588f3154dbcdc40
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-02-19 23:38:53 -0500

    connection to database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGOOS)
+  .then(() => {
+    // if connection is successful
+    console.log("Connected to MongoDB !!!!");
+  })
+  .catch((err) => {
+    // if connection is failed
+    console.log("Error: ", err);
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000 !!");
+  console.log("Server is running on port 3000 !!!!");
 });
